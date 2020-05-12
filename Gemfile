698330c799910f1e0637be36785bf9de98c3de9d
woody-yoshihiro <woody19810930@gmail.com>
2020-05-12 15:42:01 +0900

    create

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ gem "html2slim"
 gem "bootstrap"
 
 gem "rails_autolink"
+
+gem "ransack"
--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,12 @@ class Task < ApplicationRecord
   belongs_to :user
 
   scope :recent, -> { order(created_at: :desc) }
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
